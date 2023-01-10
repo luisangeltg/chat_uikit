393080b3b9aa99f0fafe65e53aae6b21190ddb02
Luis Angel Torres G <t.g.luisangel@hotmail.com>
2023-01-09 23:59:50 -0600

    Initial Commit

---
+++ ChatApp01/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ChatApp01
+//
+//  Created by Luis Angel Torres G on 09/01/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
